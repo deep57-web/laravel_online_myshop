c5592cdfe96bc4bf9ada9f2ad324fe16756363f8
deep57-web <dipudas@stegpearl.com>
2024-09-06 19:10:35 +0530

    Assest Implements

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
 {
-    //
+    
+    public function index()
+    {
+       // echo "Hello Login";
+         return view('admin.login');
+    }
+
+
+
 }
